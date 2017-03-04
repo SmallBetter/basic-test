d534c50b38f4377e99fc917dba934b679d379243
SmallBetter <forsakenevil0811@gmail.com>
2017-03-04 10:25:05 +0700

    create Hero

---
+++ src/Models/Hero.js
@@ -0,0 +1,32 @@
+export default class Hero {
+  constructor() {
+    this.name = 'newHero'
+    this.hp = 100
+    this.damage = 10
+    this.weak = false
+  }
+  setName(name) {
+    this.name = name
+  }
+  getName() {
+    return this.name
+  }
+  setHp(hp) {
+    this.hp = hp
+  }
+  getHp() {
+    return this.hp
+  }
+  setDamage(damage) {
+    this.damage = damage
+  }
+  getdamage() {
+    return this.damage
+  }
+  setWeak(weak) {
+    this.weak = weak
+  }
+  isWeak() {
+    return this.weak
+  }
+}
